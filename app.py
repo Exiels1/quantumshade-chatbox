86b00c5c7ef055e9d7b86909ae7a1ecf1021fb56
Exiels1 <oliveisfruitful@gmai.com>
2025-06-27 06:26:33 +0100

    Updated About Page and CSS

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 from flask_socketio import SocketIO, emit, join_room
 import sqlite3

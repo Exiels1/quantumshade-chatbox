28907c46f87c05db7a735e39cfaea594ce951b7a
Exiels1 <oliveisfruitful@gmai.com>
2025-08-23 14:10:14 +0100

    Fix template folder path

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
+import os
 
-# tell Flask to look for templates/static inside the app folder
+# get port from Render environment
+port = int(os.environ.get("PORT", 10000))
+
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
-
 socketio = SocketIO(app)
 
-# routes
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)

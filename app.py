6d0c205a72a7ecb9b9e8e0ee1530137ef9f3de4b
Exiels1 <oliveisfruitful@gmai.com>
2025-08-23 12:23:06 +0100

    Add app.py for Render deployment

--- app.py
+++ app.py
@@ -18,5 +18,4 @@ def handle_message(msg):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # Important: host=0.0.0.0 so Render can access it
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)

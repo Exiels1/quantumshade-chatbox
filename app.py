431bfe754d5522839189dac78f8061de8437aa0b
Exiels1 <oliveisfruitful@gmail.com>
2025-08-25 18:28:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import os
 
 app = Flask(
     __name__,
@@ -13,6 +13,6 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)

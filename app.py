48edda94cc395a1658f9db7487612f305d7d00fe
Exiels1 <oliveisfruitful@gmai.com>
2025-09-02 11:40:14 +0100

    prepare for render deploy

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-import os
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-
-app = Flask(
-    __name__,
-    template_folder=os.path.join("app", "templates"),
-    static_folder=os.path.join("app", "static")
-)
-socketio = SocketIO(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+from app import create_app, socketio
+app = create_app()
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)

8b9441cc31f5c13d49406ec3fd7f4f1ac7c58dc1
Exiels1 <oliveisfruitful@gmai.com>
2025-09-02 21:45:50 +0100

    ready for Render

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from app import create_app, socketio
 
-# Create app using factory
+# create the Flask app
 app = create_app()
 
+# for Render/production (Gunicorn or equivalent will call this "app")
+if __name__ != "__main__":
+    application = app  # sometimes Render expects "application"
+
+# for local dev
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
